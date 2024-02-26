71669941dd6767f4684bda8d0b383fbd6b9d8d55
Jorge A Peroza M <cicelcup@hotmail.com>
2024-02-26 15:15:04 -0600

    Add Generative AI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:logger/logger.dart';
 
 void main() async {
@@ -23,8 +24,18 @@ class MyApp extends StatelessWidget {
           title: const Text('Promptior'),
         ),
         body: GestureDetector(
-          onTap: () {
-            Logger().i('Test Promptior');
+          onTap: () async {
+            var gemini = dotenv.env['GEMINI'];
+            if (gemini == null) {
+              Logger().i('Error loading the access to gemini');
+            } else {
+              final model = GenerativeModel(model: 'gemini-pro', apiKey: gemini);
+
+              const prompt = '¿Qué servicios ofrece Promptior?';
+              final content = [Content.text(prompt)];
+              final response = await model.generateContent(content);
+              Logger().i(response.text);
+            }
           },
           child: const Text('Promptior'),
         ),
